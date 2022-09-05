0f7a4a2d5fff7e0bb44aa5e4c3033f8874455564
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2022-09-05 08:51:55 -0500

    Chore: Subiendo proyecto al repositorio

---
+++ tests/login.js
@@ -0,0 +1,12 @@
+const { Selector } = require("testcafe");
+
+fixture `Login Suite`.page('https://saucedemo.com/');
+
+test('Valid Login', async (t) =>{
+    await t
+    .typeText('#user-name','standard_user')
+    .typeText('#password','secret_sauce')
+    .click('#login-button')
+    .expect(Selector('.title').innerText)
+    .eql('PRODUCTS')
+})
